86a6ab2a4eefd12993f408dbbe2d8de610b4396f
ahmed elgohary <amdelgohary@gmail.com>
2017-11-15 18:50:11 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+APP = ['currencyConverter.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'plist': {
+        'LSUIElement': True,
+    },
+    'packages': ['rumps'],
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
