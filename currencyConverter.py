ca7cdc26d2092ee33c27c6dce5215fcaa29f0d28
ahmed elgohary <amdelgohary@gmail.com>
2017-11-15 18:51:09 -0700

    submenus working

--- currencyConverter.py
+++ currencyConverter.py
@@ -1,21 +1,53 @@
-import rumps
+import rumps, json, requests
+url = "https://api.fixer.io/latest?base=CAD"
+
+
+data = requests.get(url=url).json()
+all_currencies = list(data['rates'])
+rumps.debug_mode(True)
+
 
 class currencyConverter(rumps.App):
     def __init__(self):
-        super(currencyConverter, self).__init__("Currency Converter")
-        self.menu = ["Preferences", "Pick a currency"]
+        self.currencies = all_currencies
+        self.sub_menu = []
+        self.currency_menu()
+        self.selected_currency = 'USD'
+        self.selected_rate = data['rates'][self.selected_currency]
+        super(currencyConverter, self).__init__("Currency Converter", title = "CAD:" + self.selected_currency + ' %.3f' % self.selected_rate)
+
+        self.menu = [
+        'About',
+        'Preferences',
+        None,
+        ('Pick a Currency', self.sub_menu),
+        None
+        ]
+
+    def current_currency(self):
+        print('hi')
+
+    def currency_menu(self):
+        smenu = []
+        for currency in self.currencies:
+            item = (rumps.MenuItem(currency, callback=self.current_currency))
+            smenu.append(item)
+        self.sub_menu = smenu
+
+
+    #def current_rate(self):
+
+    @rumps.clicked("About")
+    def about(self, sender):
+        rumps.alert("This is a currency conversion app created by Ahmed El Gohary. \n It currently defaults to base CAD, I will later add an option that makes it possible to pick any desired base currency.")
 
     @rumps.clicked("Preferences")
     def prefs(self, _):
         rumps.alert("jk! no preferences available!")
 
-    @rumps.clicked("Silly button")
-    def onoff(self, sender):
-        sender.state = not sender.state
-
-    @rumps.clicked("Say hi")
-    def sayhi(self, _):
-        rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
 
 if __name__ == "__main__":
     currencyConverter().run()
+
+# add red for decrease, green for increase
+# option for quantity
