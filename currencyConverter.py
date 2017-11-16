341998d62555a8817f4383b6231f221315d4452c
ahmed elgohary <amdelgohary@gmail.com>
2017-11-15 22:45:10 -0700

    changing currencies works

--- currencyConverter.py
+++ currencyConverter.py
@@ -10,11 +10,14 @@ rumps.debug_mode(True)
 class currencyConverter(rumps.App):
     def __init__(self):
         self.currencies = all_currencies
-        self.sub_menu = []
-        self.currency_menu()
         self.selected_currency = 'USD'
         self.selected_rate = data['rates'][self.selected_currency]
-        super(currencyConverter, self).__init__("Currency Converter", title = "CAD:" + self.selected_currency + ' %.3f' % self.selected_rate)
+
+        self.sub_menu = []
+        self.currency_menu()
+
+        super(currencyConverter, self).__init__("Currency Converter", title =
+        "CAD:" + self.selected_currency + ' %.3f' % self.selected_rate)
 
         self.menu = [
         'About',
@@ -24,27 +27,29 @@ class currencyConverter(rumps.App):
         None
         ]
 
-    def current_currency(self):
-        print('hi')
-
     def currency_menu(self):
-        smenu = []
+        # builds the menu
         for currency in self.currencies:
-            item = (rumps.MenuItem(currency, callback=self.current_currency))
-            smenu.append(item)
-        self.sub_menu = smenu
+            item = rumps.MenuItem(currency, callback = self.change_currency)
+            item.state = 0
+            self.sub_menu.append(item)
 
 
-    #def current_rate(self):
+    def change_currency(self, sender):
+        # changes to other currency if user changes it from the menu
+        self.selected_currency = sender.title
+        self.selected_rate = data['rates'][self.selected_currency]
 
-    @rumps.clicked("About")
+        super(currencyConverter, self).__init__("Currency Converter", title =
+        "CAD:" + self.selected_currency + ' %.3f' % self.selected_rate)
+
+    @rumps.clicked('About')
     def about(self, sender):
         rumps.alert("This is a currency conversion app created by Ahmed El Gohary. \n It currently defaults to base CAD, I will later add an option that makes it possible to pick any desired base currency.")
 
-    @rumps.clicked("Preferences")
+    @rumps.clicked('Preferences')
     def prefs(self, _):
-        rumps.alert("jk! no preferences available!")
-
+        rumps.alert("preferences to be added later!")
 
 if __name__ == "__main__":
     currencyConverter().run()
